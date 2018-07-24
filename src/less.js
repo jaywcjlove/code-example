4c54514dc685d8ac99014b78870cd1a920502128
jaywcjlove <398188662@qq.com>
2018-07-25 02:48:29 +0800

    Add less code example.

---
+++ src/less.js
@@ -0,0 +1,26 @@
+
+const code = `@base: #f938ab;
+
+@width: 10px;
+@height: @width + 10px;
+
+#header {
+  width: @width;
+  height: @height;
+}
+
+.box-shadow(@style, @c) when (iscolor(@c)) {
+  -webkit-box-shadow: @style @c;
+  box-shadow:         @style @c;
+}
+.box-shadow(@style, @alpha: 50%) when (isnumber(@alpha)) {
+  .box-shadow(@style, rgba(0, 0, 0, @alpha));
+}
+.box {
+  color: saturate(@base, 5%);
+  border-color: lighten(@base, 30%);
+  div { .box-shadow(0 0 5px, 30%) }
+}
+`;
+
+export default code;
